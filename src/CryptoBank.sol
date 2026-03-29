778a6a698a7d32b9be5519c728e9f1526cef47fa
Thomas Sorza <sthx09567541@gmail.com>
2026-03-29 16:35:35 -0500

    creaded basic structure

--- src/CryptoBank.sol
+++ src/CryptoBank.sol
@@ -15,4 +15,29 @@ pragma solidity ^0.8.33;
 
 contract CryptoBank{
 
+    address public owner;
+    uint256 public maxDeposit;
+    mapping(address => uint) public balance;
+
+    event EtherDeposited(uint256 ethAmount_, address account_);
+
+    constructor (uint256 maxDeposit_) {
+        owner = msg.sender;
+        maxDeposit = maxDeposit_;
+    }
+
+    function deposit() external payable {
+        balance[msg.sender] += msg.value;
+        emit EtherDeposited(msg.value, msg.sender);
+    } 
+
+    function withdraw(uint256 amount_) public {
+        require(amount_ <= balance[msg.sender], "not enough eth to transfer");
+    
+        balance[msg.sender] -= amount_;
+
+        (bool successs, ) = msg.sender.call{value : amount_}("");
+        require(successs, "Transaction failed")
+    }
+
 }
