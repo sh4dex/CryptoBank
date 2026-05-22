9080c365a76da15f6982d04254d0d08924efd366
Thomas Sorza <sthx09567541@gmail.com>
2026-05-21 22:02:05 -0500

    built: Reentrancy protection & send to other addresses

--- script/CryptoBankDeployScript.s.sol
+++ script/CryptoBankDeployScript.s.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.30;
+pragma solidity 0.8.33;
 
 // import {console} from "forge-std/console.sol";
 import {Script} from "forge-std/Script.sol";
-import {CryptoBank} from  "../src/CryptoBank.sol";
+import {CryptoBank} from "../src/CryptoBank.sol";
 
 contract CryptoBankDeploy is Script {
 
-    function run()  external {
+    //TODO: Deploy on testnet
+    function run() external {
         vm.startBroadcast();
 
         CryptoBank cryptoBank = new CryptoBank(10 ether, msg.sender);
--- src/CryptoBank.sol
+++ src/CryptoBank.sol
@@ -1,67 +1,126 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.33;
+pragma solidity 0.8.33;
 
-// Functions:
-    //1. Deposit ether
-    //2. Withdraw ether
+/**
+ * @title CryptoBank
+ * @author Thomas Sorza (Sh4dex)
+ * @notice Simple implementation of a Multi user crypto bank, smart contract act as a registry of the balances of each address,
+ * It has funds limits for all addresses with hand made access control system for contract onwner.
+ * Allow operations: Deposit & withdraw ether
+ * NOTE: User can only withdraw previusly depsited ether
+ */
 
-//Rules:
-    //1. Multiuser
-    //2. Only can deposit ether
-    //3. User can only withdraw previusly depsited ether
-    //4. Max deposit
-    //5. Max deposit modified by owner
-
-contract CryptoBank{
-
+contract CryptoBank {
     address public immutable I_OWNER;
     uint256 public depositLimit;
+    bool private _locked;
     mapping(address => uint256) public balances;
 
-    event EtherDeposited(uint256 ethAmount_, address account_);
-    event EtherWithdrawn(uint256 ethAmount_, address userOrigin_);
-    event MaxBalanceEdited(uint256 newLimit_);
+    event EtherDeposited(uint256 indexed ethAmount_, address indexed account_);
+    event EtherWithdrawn(uint256 indexed ethAmount_, address indexed userOrigin_);
+    event EtherSent(uint256 indexed ethAmount_, address indexed userOrigin_, address indexed userDestination_);
+    event MaxBalanceEdited(uint256 indexed newLimit_);
 
+    /**
+     * @dev modifier that execute the {_onlyOwner} function before proceed with other execution
+     */
     modifier onlyOwner() {
         _onlyOwner();
         _;
     }
 
+    /**
+     * @dev modifier that executes the {_checkTransferAmount} function before proceed with other execution
+     * @param amount_ amount to transfer
+     */
+    modifier checkTransferAmount(uint256 amount_) {
+        _checkTransferAmount(amount_);
+        _;
+    }
+
+    /**
+     * @dev To avoid reentrancy attacks the contract manage a state with ´_locked´ where it should be not locked indicading
+     * that the call is not in process, while call is in process do not allow to fire callbacks.
+     * NOTE: if the fuction related with ´_´ reverts, ´_locked´ reverts too. (reverts to {false})
+     */
+    modifier nonReentrant() {
+        require(!_locked, "Reentrant call");
+        _locked = true;
+        _;
+        _locked = false;
+    }
+
+    /**
+     * @dev checks if the amount is greater than zero and is on the balance of the msg.sender
+     * @param amount_ amount to transfer (should be into the balance mapping)
+     */
+    function _checkTransferAmount(uint256 amount_) internal view {
+        require(amount_ <= balances[msg.sender] && amount_ > 0, "not enough eth to transfer");
+    }
+
+    /**
+     * @dev validate that the sender is contract owner
+     */
     function _onlyOwner() internal view {
         require(msg.sender == I_OWNER, "Not Allowed!");
     }
 
-    constructor (uint256 depositLimit_, address admin_) {
+    /**
+     * @param depositLimit_ Initial deposit limit per user
+     * @param admin_ Unique admin set at contract's deployment
+     */
+    constructor(uint256 depositLimit_, address admin_) {
         I_OWNER = admin_;
         depositLimit = depositLimit_;
     }
 
+    /**
+     * @dev deposit ether into it's account updating ´balances()´, only if ´depositLimit´ is not exceeded
+     */
     function depositEther() external payable {
+        require(msg.value > 0, "Deposit value can't be zero");
         require(balances[msg.sender] + msg.value <= depositLimit, "Max Balance reached, please withdraw.");
-        require(msg.value >= 0, "Deposit value can't be zero");
         balances[msg.sender] += msg.value;
         emit EtherDeposited(msg.value, msg.sender);
-    } 
+    }
 
-
-    //TODO: Add reentrancy protection (locked state)
-    function withdrawEther(uint256 amount_) public {
-        require(amount_ <= balances[msg.sender] && amount_>0, "not enough eth to transfer");
-    
+    /**
+     * Functions that indicates how to
+     * @param amount_ amount to withdraw from own balance
+     * @dev using {nonReentrant} to avoid Reentrancy attacks
+     */
+    function withdrawEther(uint256 amount_) public checkTransferAmount(amount_) nonReentrant(){
         balances[msg.sender] -= amount_;
 
-        (bool success, ) = msg.sender.call{value : amount_}("");
-        require(success, "Transaction failed");
+        (bool success,) = msg.sender.call{value: amount_}("");
+        require(success, "Transfer Failed!");
 
         emit EtherWithdrawn(amount_, msg.sender);
     }
 
-    function modifyMaxBalance(uint256 newLimit_) external onlyOwner{
+    /**
+     * @param newLimit_ new limit of funds per user
+     * @dev Only owner can call this function
+     */
+    function modifyMaxLimit(uint256 newLimit_) external onlyOwner {
+        require(newLimit_ > 0, "You can't set zero as new limit");
         depositLimit = newLimit_;
         emit MaxBalanceEdited(newLimit_);
     }
 
+    /**
+     * Send Ether from own balance to other user (address)
+     * @param amount_  amount of eth to send
+     * @param to_ receiver address
+     */
+    function sendEth(uint256 amount_, address to_) public checkTransferAmount(amount_) nonReentrant() {
+        require(balances[to_] + amount_ <= depositLimit, "Max balance reached for receiver");
+        balances[msg.sender] -= amount_;
+        balances[to_] += amount_;
 
-    //TODO: Send Ether to other user
+        (bool success,) = to_.call{value: amount_}("");
+        require(success, "Transfer failed!");
+        emit EtherSent(amount_, msg.sender, to_);
+    }
 }
